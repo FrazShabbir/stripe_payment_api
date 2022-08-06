51ce447ad1710f03b258d969a29e6ca6981fbf13
Fraz Shabbir <fraz.shabbir54@gmail.com>
2022-08-06 22:20:01 +0500

    Update index.blade.php

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -1,201 +1,199 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="./style.css">
-  <style>
-    body {
-    font-family: 'Roboto', sans-serif!important;
-	margin:0;
-	padding:0;
-	box-sizing: border-box;
-}
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+    <style>
+        body {
+            font-family: 'Roboto', sans-serif !important;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
 
-.mainscreen
-{
-	min-height: 100vh;
-	width: 100%;
-	display: flex;
-    flex-direction: column;
-    background-color: #DFDBE5;
-    background-image: url("https://wallpaperaccess.com/full/3063067.png");
-    color:#963E7B;
-}
+        .mainscreen {
+            min-height: 100vh;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            background-color: #DFDBE5;
+            background-image: url("https://wallpaperaccess.com/full/3063067.png");
+            color: #963E7B;
+        }
 
-.card {
-	width: 60rem;
-    margin: auto;
-    background: white;
-    position:center;
-    align-self: center;
-    top: 50rem;
-    border-radius: 1.5rem;
-    box-shadow: 4px 3px 20px #3535358c;
-    display:flex;
-    flex-direction: row;
-    
-}
+        .card {
+            width: 60rem;
+            margin: auto;
+            background: white;
+            position: center;
+            align-self: center;
+            top: 50rem;
+            border-radius: 1.5rem;
+            box-shadow: 4px 3px 20px #3535358c;
+            display: flex;
+            flex-direction: row;
 
-.leftside {
-	background: #030303;
-	width: 25rem;
-	display: inline-flex;
-    align-items: center;
-    justify-content: center;
-	border-top-left-radius: 1.5rem;
-    border-bottom-left-radius: 1.5rem;
-}
+        }
 
-.product {
-    object-fit: cover;
-	width: 20em;
-    height: 20em;
-    border-radius: 100%;
-}
+        .leftside {
+            background: #030303;
+            width: 25rem;
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            border-top-left-radius: 1.5rem;
+            border-bottom-left-radius: 1.5rem;
+        }
 
-.rightside {
-    background-color: #ffffff;
-	width: 35rem;
-	border-bottom-right-radius: 1.5rem;
-    border-top-right-radius: 1.5rem;
-    padding: 1rem 2rem 3rem 3rem;
-}
+        .product {
+            object-fit: cover;
+            width: 20em;
+            height: 20em;
+            border-radius: 100%;
+        }
 
-p{
-    display:block;
-    font-size: 1.1rem;
-    font-weight: 400;
-    margin: .8rem 0;
-}
+        .rightside {
+            background-color: #ffffff;
+            width: 35rem;
+            border-bottom-right-radius: 1.5rem;
+            border-top-right-radius: 1.5rem;
+            padding: 1rem 2rem 3rem 3rem;
+        }
 
-.inputbox
-{
-    color:#030303;
-	width: 100%;
-    padding: 0.5rem;
-    border: none;
-    border-bottom: 1.5px solid #ccc;
-    margin-bottom: 1rem;
-    border-radius: 0.3rem;
-    font-family: 'Roboto', sans-serif;
-    color: #615a5a;
-    font-size: 1.1rem;
-    font-weight: 500;
-  outline:none;
-}
+        p {
+            display: block;
+            font-size: 1.1rem;
+            font-weight: 400;
+            margin: .8rem 0;
+        }
 
-.expcvv {
-    display:flex;
-    justify-content: space-between;
-    padding-top: 0.6rem;
-}
+        .inputbox {
+            color: #030303;
+            width: 100%;
+            padding: 0.5rem;
+            border: none;
+            border-bottom: 1.5px solid #ccc;
+            margin-bottom: 1rem;
+            border-radius: 0.3rem;
+            font-family: 'Roboto', sans-serif;
+            color: #615a5a;
+            font-size: 1.1rem;
+            font-weight: 500;
+            outline: none;
+        }
 
-.expcvv_text{
-    padding-right: 1rem;
-}
-.expcvv_text2{
-    padding:0 1rem;
-}
+        .expcvv {
+            display: flex;
+            justify-content: space-between;
+            padding-top: 0.6rem;
+        }
 
-.button{
-    background: linear-gradient(
-135deg
-, #753370 0%, #298096 100%);
-    padding: 15px;
-    border: none;
-    border-radius: 50px;
-    color: white;
-    font-weight: 400;
-    font-size: 1.2rem;
-    margin-top: 10px;
-    width:100%;
-    letter-spacing: .11rem;
-    outline:none;
-}
+        .expcvv_text {
+            padding-right: 1rem;
+        }
 
-.button:hover
-{
-	transform: scale(1.05) translateY(-3px);
-    box-shadow: 3px 3px 6px #38373785;
-}
+        .expcvv_text2 {
+            padding: 0 1rem;
+        }
 
-@media only screen and (max-width: 1000px) {
-    .card{
-        flex-direction: column;
-        width: auto;
-      
-    }
+        .button {
+            background: linear-gradient(135deg, #753370 0%, #298096 100%);
+            padding: 15px;
+            border: none;
+            border-radius: 50px;
+            color: white;
+            font-weight: 400;
+            font-size: 1.2rem;
+            margin-top: 10px;
+            width: 100%;
+            letter-spacing: .11rem;
+            outline: none;
+        }
 
-    .leftside{
-        width: 100%;
-        border-top-right-radius: 0;
-        border-bottom-left-radius: 0;
-      border-top-right-radius:0;
-      border-radius:0;
-    }
+        .button:hover {
+            transform: scale(1.05) translateY(-3px);
+            box-shadow: 3px 3px 6px #38373785;
+        }
 
-    .rightside{
-        width:auto;
-        border-bottom-left-radius: 1.5rem;
-        padding:0.5rem 3rem 3rem 2rem;
-      border-radius:0;
-    }
-}
-  </style>
+        @media only screen and (max-width: 1000px) {
+            .card {
+                flex-direction: column;
+                width: auto;
+
+            }
+
+            .leftside {
+                width: 100%;
+                border-top-right-radius: 0;
+                border-bottom-left-radius: 0;
+                border-top-right-radius: 0;
+                border-radius: 0;
+            }
+
+            .rightside {
+                width: auto;
+                border-bottom-left-radius: 1.5rem;
+                padding: 0.5rem 3rem 3rem 2rem;
+                border-radius: 0;
+            }
+        }
+    </style>
 </head>
+
 <body>
-  
-  <div class="mainscreen">
-    <!-- <img src="https://image.freepik.com/free-vector/purple-background-with-neon-frame_52683-34124.jpg"  class="bgimg " alt="">--> 
-      <div class="card">
-        <div class="leftside">
-          <img
-            src="https://i.pinimg.com/originals/18/9d/dc/189ddc1221d9c1c779dda4ad37a35fa1.png"
-            class="product"
-            alt="Shoes"
-          />
-        </div>
-        <div class="rightside">
-            <form role="form" action="{{ route('stripe.post') }}" method="post"
-            class="require-validation" data-cc-on-file="false"
-            data-stripe-publishable-key="{{ env('STRIPE_KEY') }}" id="payment-form"> @csrf
-            {{ @method_field('POST') }}
 
-            <h1>Make Your Payment Here</h1>
-            {{-- <h2>Payment Information</h2> --}}
-            <p>Cardholder Name</p>
-            <input type="text" class="inputbox" name="name" required placeholder="Name on Card" />
-            <p>Account No</p>
-            <input type="text" class="inputbox" name="acc_no" required placeholder="Enter your Account Number for Refund"  />
-            <p>Amount</p>
-            <input type="number" step="0.01" class="inputbox" name="amount" required placeholder="Enter the Amount"/>
-
-
-            <p>Cardholder Name</p>
-            <div id="card-element">
-              <!-- A Stripe Element will be inserted here. -->
+    <div class="mainscreen">
+        <!-- <img src="https://image.freepik.com/free-vector/purple-background-with-neon-frame_52683-34124.jpg"  class="bgimg " alt="">-->
+        <div class="card">
+            <div class="leftside">
+                <img src="{{ asset(fromSettings('logo')) ?? 'https://i.pinimg.com/originals/18/9d/dc/189ddc1221d9c1c779dda4ad37a35fa1.png' }}"
+                    class="product" alt="{{ fromSettings('short_title') }}"
+                    style="max-width: 200px;max-height: 200px" />
             </div>
-            <div id="card-errors" role="alert"></div>
-       
-            <div class="expcvv">
+            <div class="rightside">
+                <form role="form" action="{{ route('stripe.post') }}" method="post" class="require-validation"
+                    data-cc-on-file="false" data-stripe-publishable-key="{{ env('STRIPE_KEY') }}" id="payment-form">
+                    @csrf
+                    {{ @method_field('POST') }}
 
-        
+                    <h1>Make Your Payment Here</h1>
+                    {{-- <h2>Payment Information</h2> --}}
+                    <p>Cardholder Name</p>
+                    <input type="text" class="inputbox" name="name" required placeholder="Name on Card" />
+                    <p>Account No</p>
+                    <input type="text" class="inputbox" name="acc_no" required
+                        placeholder="Enter your Account Number for Refund" />
+                    <p>Amount</p>
+                    <input type="number" step="0.01" class="inputbox" name="amount" required
+                        placeholder="Enter the Amount" />
+
+
+                    <p>Cardholder Name</p>
+                    <div id="card-element">
+                        <!-- A Stripe Element will be inserted here. -->
+                    </div>
+                    <div id="card-errors" role="alert"></div>
+
+                    <div class="expcvv">
+
+
+                    </div>
+                    <p></p>
+                    <button class="button" type="submit">Submit Payment</button>
+                </form>
+            </div>
         </div>
-            <p></p>
-            <button class="button" type="submit">Submit Payment</button>
-          </form>
-        </div>
-      </div>
     </div>
-  
+
     <script src="https://js.stripe.com/v3/"></script>
     <script>
         var stripe = Stripe(
             'pk_test_51L8rx2GUAa86j6rclhVLBSvunSYXrkGTP8PL6pIBEvlgh333tB4gTxgds86W9hDXECeimzGGsiauJzlQftUT5aHv00U2SGSwcv'
-            );
+        );
         var elements = stripe.elements();
         // Custom styling can be passed to options when creating an Element.
         var style = {
@@ -241,6 +239,7 @@ p{
             form.submit();
         }
     </script>
-       @include('sweetalert::alert')
+    @include('sweetalert::alert')
 </body>
+
 </html>
